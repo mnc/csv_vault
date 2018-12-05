7d987a1e22da66db13e7dbae27aa870943f4b329
Nobuya SASAKI <manchose9@gmail.com>
2018-12-06 03:07:40 +0900

    PoC. Implement encrypt CSV method and decrypt CSV method

--- lib/csv_vault.rb
+++ lib/csv_vault.rb
@@ -1,5 +1,8 @@
 require "csv_vault/version"
+require "csv_vault/main"
 
 module CsvVault
-  # Your code goes here...
+  def self.from_file(input_file:, output_file: nil, col_nums: [], passphrase:, salt: nil)
+    CsvVault::Main.new(input_file: input_file, output_file: output_file, col_nums: col_nums, passphrase: passphrase, salt: salt)
+  end
 end
---
+++ lib/csv_vault/main.rb
@@ -0,0 +1,77 @@
+require 'csv'
+require 'active_support'
+
+module CsvVault
+  class Main
+    def initialize(input_file:, output_file: nil, col_nums: [], passphrase:, salt: nil)
+      @input_file  = input_file
+      @output_file = output_file
+      @col_nums    = col_nums
+      @passphrase  = passphrase
+      @salt        = salt.to_s
+      @cryptor     = cryptor
+    end
+
+    def encrypt
+      @processed_rows = CSV.foreach(@input_file).map do |row|
+        if @col_nums.empty?
+          encrypt_all(row)
+        else
+          encrypt_partially(row)
+        end
+      end
+      self
+    end
+
+    def output
+      CSV.open(@output_file, 'w') do |csv|
+        @processed_rows.each do |encrypted_row|
+          csv << encrypted_row
+        end
+      end
+    end
+
+    def decrypt
+      @processed_rows = CSV.foreach(@input_file).map do |row|
+        if @col_nums.empty?
+          decrypt_all(row)
+        else
+          decrypt_partially(row)
+        end
+      end
+      self
+    end
+
+    private
+
+    # TODO: parameterize length, digest and cipher
+    def cryptor
+      key = ActiveSupport::KeyGenerator.new(@passphrase).generate_key(@salt, 32)
+      ActiveSupport::MessageEncryptor.new(key, cipher: "aes-256-cbc", digest: "SHA256")
+    end
+
+    def encrypt_all(row)
+      row.map do |column_data|
+        @cryptor.encrypt_and_sign(column_data)
+      end
+    end
+
+    def encrypt_partially(row)
+      row.map.with_index do |column_data, i|
+        @col_nums.include?(i) ? @cryptor.encrypt_and_sign(column_data) : column_data
+      end
+    end
+
+    def decrypt_all(row)
+      row.map do |column_data|
+        @cryptor.decrypt_and_verify(column_data)
+      end
+    end
+
+    def decrypt_partially(row)
+      row.map.with_index do |column_data, i|
+        @col_nums.include?(i) ? @cryptor.decrypt_and_verify(column_data) : column_data
+      end
+    end
+  end
+end
